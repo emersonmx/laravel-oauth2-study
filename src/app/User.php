4ec8a9f69d938ae94e3bb106e822aa2e498b27f4
Emerson MX <emersonmx@gmail.com>
2017-08-21 22:13:46 -0300

    Finish OAuth2 config

--- src/app/User.php
+++ src/app/User.php
@@ -2,12 +2,14 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
